ceb00645c7513bf0140921f08174ad3c850a54de
magnusziegler-cap <magnus.ziegler@capgemini.com>
2022-10-18 12:46:09 +0200

    add parse_bathrooms, clean_listings to utils

--- scripts/utilities.py
+++ scripts/utilities.py
@@ -1,51 +1,151 @@
-import pandas as pd
-import numpy as np
-import gzip
-import shutil
 import os
-import geojson
+import shutil
+import gzip
 
-def restructure_data():
+import geojson
+import pandas as pd
+
+from typing import Union
+
+# Globals
+DATA_PATH = '../data'
+
+FILE_LIST = {
+    'calendar':'calendar.csv',
+    'calendar_gzip':'calendar.csv.gz',
+    'listings':'listings.csv',
+    'listings_summary':'listings_summary.csv',
+    'listings_gzip':'listings.csv.gz',
+    'reviews':'reviews.csv',
+    'reviews_summary':'reviews_summary.csv',
+    'reviews_gzip':'reviews.csv.gz',
+    'neighbourhoods':'neighbourhoods.csv',
+    'neighbourhoods_geojson':'neighbourhoods.geojson',
+}
+
+def create_backup(data_path=None) -> None:
+    """ Creates a secondary or backup copy of the original data before any restructuring takes place
+    """
+
+    if data_path is None:
+        data_path = DATA_PATH
+
+    backup_path = os.path.join(data_path,'backup')
+    os.mkdir(backup_path)
+
+    for file_name in FILE_LIST.values():
+        shutil.copy(
+            src=os.path.join(data_path, file_name),
+            dst=os.path.join(backup_path, file_name)
+            )
+
+
+def restructure_data(data_path=None) -> None:
     """ Renames the downloaded summary data and unzips the calendar, listings, and reviews data
     """
-    data_path = '../data/'
+    if data_path is None:
+        data_path = DATA_PATH
 
     # rename the summary-packages with "_summary" postfix
-    os.rename((data_path+'reviews.csv'), (data_path+'reviews_summary.csv'))
-    os.rename((data_path+'listings.csv'), (data_path+'listings_summary.csv'))
+    os.rename(
+        src=os.path.join(data_path,'reviews.csv'),
+        dst=os.path.join(data_path,'reviews_summary.csv'))
+    os.rename(
+        src=os.path.join(data_path,'listings.csv'),
+        dst=os.path.join(data_path,'listings_summary.csv'))
 
     # unzip packages
     in_names = ["calendar.csv.gz", "listings.csv.gz", "reviews.csv.gz"]
     out_names = ["calendar.csv", "listings.csv", "reviews.csv"]
 
     for in_name, out_name in zip(in_names, out_names):
-        with gzip.open(data_path + in_name, 'rb') as f_in:
-            with open(data_path + out_name, 'wb') as f_out:
+        with gzip.open(os.path.join(data_path,in_name), 'rb') as f_in:
+            with open(os.path.join(data_path,out_name), 'wb') as f_out:
                 shutil.copyfileobj(f_in, f_out)
 
-def load_data():
+def load_data(data_path=None):
     """Loads all data
 
     Returns:
-        _type_: dataframes:
+        pd.DataFrame:
         calendar
         listings
         listings_summary
         neighbourhoods
         reviews
         reviews_summary
-        _type_: geojson feature_collection
-        gj_neighbourhoods
 
+        geojson:
+        feature_collection:gj_neighbourhoods
     """
-    data_path = '../data/'
-    df_calendar = pd.read_csv(filepath_or_buffer=data_path+'calendar.csv')
-    df_listings = pd.read_csv(filepath_or_buffer=data_path+'listings.csv')
-    df_listings_summary = pd.read_csv(filepath_or_buffer=data_path+'listings_summary.csv')
-    df_neighbourhoods = pd.read_csv(filepath_or_buffer=data_path+'neighbourhoods.csv')
-    df_reviews = pd.read_csv(filepath_or_buffer=data_path+'reviews.csv')
-    df_reviews_summary = pd.read_csv(filepath_or_buffer=data_path+'reviews_summary.csv')
 
-    with open(data_path+'neighbourhoods.geojson', 'rb') as f_in:
-        gj_neighbourhoods = geojson.load(f_in) 
+    if data_path is None:
+        data_path = DATA_PATH
+
+    df_calendar = pd.read_csv(filepath_or_buffer=os.path.join(data_path,'calendar.csv'))
+    df_listings = pd.read_csv(filepath_or_buffer=os.path.join(data_path,'listings.csv'))
+    df_listings_summary = pd.read_csv(filepath_or_buffer=os.path.join(data_path,'listings_summary.csv'))
+    df_neighbourhoods = pd.read_csv(filepath_or_buffer=os.path.join(data_path,'neighbourhoods.csv'))
+    df_reviews = pd.read_csv(filepath_or_buffer=os.path.join(data_path,'reviews.csv'))
+    df_reviews_summary = pd.read_csv(filepath_or_buffer=os.path.join(data_path,'reviews_summary.csv'))
+
+    with open(os.path.join(data_path,'neighbourhoods.geojson'), 'rb') as f_in:
+        gj_neighbourhoods = geojson.load(f_in)
     return df_calendar, df_listings, df_listings_summary, df_neighbourhoods, gj_neighbourhoods, df_reviews, df_reviews_summary
+
+def clean_listings(df_listings:pd.DataFrame)->pd.DataFrame:
+    """ Clean up the listings dataframe.
+    Performs the following:
+
+    Args:
+        df_listings (pd.DataFrame): listings dataframe
+
+    Returns:
+        pd.DataFrame: cleaned listings dataframe
+    """
+    df_listings_cleaned = df_listings.replace(to_replace={'t':1, 'f':0}, inplace=True) #binarize t/f
+
+    bathroom_mappings = parse_bathrooms(dataframe=df_listings["bathrooms_text"])
+    df_listings_cleaned["bathrooms"] = df_listings_cleaned["bathrooms"].replace(to_replace=bathroom_mappings, inplace=True)
+
+    return df_listings_cleaned
+
+def parse_bathrooms(dataframe:Union[pd.DataFrame,pd.Series], verbose=False)->dict:
+    """Converts bathroom text descriptions to floats,
+    and returns a dict that can be used in mapping
+
+    Args:
+        df (pd.DataFrame): listings dataframe
+        verbose (bool, optional): prints descriptions and their inferred values. Defaults to False.
+
+    Returns:
+        dict: mapping between bathroom descriptions and numbers
+    """
+    bathroom_descriptions = dataframe.unique()
+    
+    bathrooms = {}
+    bathrooms.update({'nan':float(0)}) #specifically add nan to the mapping dict
+
+    for description in bathroom_descriptions:
+        try:
+            words = description.split(' ')
+            numbers=[]
+            for word in words:
+                try:
+                    numbers.append(float(word))
+                except ValueError:
+                    pass
+            
+            if not numbers: #if its empty bathrooms = 0
+                bathroom_number = float(0)
+            else:
+                bathroom_number = numbers[0]
+            bathrooms.update({description:bathroom_number})
+
+            if verbose:
+                print(f"Text description:'{description}' has {bathroom_number} baths \n")
+
+        except AttributeError:
+            pass
+
+    return bathrooms
